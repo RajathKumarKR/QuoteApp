b0c9f19898a1010df96d444d37603cd121e7d05b
Rajath Kotebailu Raghuchandra <M1037717@mindtree.com>
2017-04-27 23:37:13 +0530

    index and html page addded

---
+++ index.js
@@ -0,0 +1,9 @@
+var express=require("express");
+var app=express();
+app.get('/',function(request,response){
+  response.sendFile(__dirname+'/index.html');
+});
+var port=3000;
+var server=app.listen(port,function(req,res){
+  console.log("Runninga at port ",port);
+})
